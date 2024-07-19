9dd34ae26d951c7e72deac8aa76da4eabfe11add
Dylan <bollarodylan@gmail.com>
2024-07-19 22:00:58 +0200

    rollback dockerfile

--- src/auth/strategies/rt.strategy.ts
+++ src/auth/strategies/rt.strategy.ts
@@ -1,31 +1,32 @@
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Request } from 'express';
-import { ForbiddenException, Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { JwtPayload, JwtPayloadWithRt } from '../types';
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { Request } from "express";
+import { ForbiddenException, Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtPayload, JwtPayloadWithRt } from "../types";
 
 @Injectable()
-export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor(private readonly configService: ConfigService) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: configService.get<string>('RT_SECRET'),
-            passReqToCallback: true,
-        });
-    }
+export class RtStrategy extends PassportStrategy(Strategy, "jwt-refresh") {
+	constructor(private readonly configService: ConfigService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			secretOrKey: configService.get<string>("RT_SECRET"),
+			passReqToCallback: true
+		});
+	}
 
-    validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
-        const refreshToken = req
-            ?.get('authorization')
-            ?.replace('Bearer', '')
-            .trim();
+	validate(req: Request, payload: JwtPayload): JwtPayloadWithRt {
+		const refreshToken = req
+			?.get("authorization")
+			?.replace("Bearer", "")
+			.trim();
 
-        if (!refreshToken) throw new ForbiddenException('Refresh token malformed');
+		if (!refreshToken)
+			throw new ForbiddenException("Refresh token malformed");
 
-        return {
-            ...payload,
-            refreshToken,
-        };
-    }
+		return {
+			...payload,
+			refreshToken
+		};
+	}
 }
