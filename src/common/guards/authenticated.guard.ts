eb9887124b5375a94dac9bdd52817d322247f2f5
Bollaro Dylan <bollarodylan@gmail.com>
2025-03-18 20:27:52 +0100

    add api key admin for scrapping

--- src/common/guards/authenticated.guard.ts
+++ src/common/guards/authenticated.guard.ts
@@ -1,9 +1,13 @@
-import { ExecutionContext, Injectable, CanActivate } from '@nestjs/common'
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
+    private readonly API_KEY = process.env.API_KEY || 'secret-api-key'
+
     async canActivate(context: ExecutionContext) {
         const request = context.switchToHttp().getRequest()
+        const apiKey = request.headers['x-api-key']
+        if (apiKey === this.API_KEY) return true
         return request.isAuthenticated()
     }
 }
